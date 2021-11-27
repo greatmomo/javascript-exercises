782eec63aed09ce421ae7ff66fc076ab4b31a31e
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-11-27 20:10:04 +0100

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, inputNum) {
+    if (inputNum < 0) {
+        return `ERROR`;
+    }
+    let outputString = ``;
+    for (let i = 0; i < inputNum; i++) {
+        outputString += inputString;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
