96a03d0efb80bd3360f35bd0d75a2761dfcec3a4
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-11-27 20:42:53 +0100

    removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(startingArray, ...theArgs) {
+    for (let i = 0; i < startingArray.length; i++) {
+        theArgs.forEach(function(innerItem, innerIndex) {
+            if (innerItem === startingArray[i]) {
+                startingArray.splice(i, 1);
+                i--;
+            }
+        });
+    }
+    return startingArray;
 };
 
 // Do not edit below this line
