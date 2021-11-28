faa2088b91af669f2a5e46e5376780da71a71950
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-11-28 14:47:01 +0100

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(fromValue, toValue) {
+    let returnSum = 0;
 
+    // No negative numbers allowed
+    if (fromValue < 0 || toValue < 0) {
+        return "ERROR";
+    }
+        
+    // No non-number allowed
+    if (typeof fromValue != "number" || typeof toValue != "number") {
+        return "ERROR";
+    }
+
+    if (toValue < fromValue) {
+        for (let i = toValue; i <= fromValue; i++) {
+            returnSum += i;
+        }
+    } else {
+        for (let i = fromValue; i <= toValue; i++) {
+            returnSum += i;
+        }
+    }
+    return returnSum;
 };
 
 // Do not edit below this line
