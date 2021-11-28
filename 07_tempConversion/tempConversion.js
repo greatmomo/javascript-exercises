c73025c50baf86b520203540cd02c15992fc962c
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-11-28 14:56:48 +0100

    tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(fIn) {
+  // Math.round(n * 10) / 10;
+  return Math.round((fIn - 32) * 5 / 9 * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cIn) {
+  return Math.round((cIn * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
