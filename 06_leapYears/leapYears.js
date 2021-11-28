48c5792d63336ade812029f4830f04a22c285c2f
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-11-28 14:51:03 +0100

    leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    // 400 is true
+    // 100 is false
+    // 4 is true
+    if (inputYear % 400 == 0) {
+        return true;
+    } else if (inputYear % 100 == 0) {
+        return false;
+    } else if (inputYear % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
