fae5f84b89bb182f8800e800d7ea9b696650a14a
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-12-08 12:14:54 +0100

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	let sum = 0;
+  inputArray.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  let total = 1;
+  inputArray.forEach(element => {
+    total *= element;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let pow = a;
+  if (b === 0) {
+    return 1;
+  }
+	for (let i = 2; i <= b; i++) {
+    pow *= a;
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let factorial = 1;
+  for (let i = 2; i <= n; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
